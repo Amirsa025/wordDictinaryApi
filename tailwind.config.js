104d2024d62673e3137ca446667c7adc12239b85
Amirsa025 <amirsaki06@gmail.com>
2023-01-31 14:06:44 +0330

    inital project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Cooper': ['Cooper Black', 'sans-serif'],
+        'Nunito': ['Nunito Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
